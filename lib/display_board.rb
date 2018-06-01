2765a7c2d4a5531b3d4fdb96f2c639b0cf9985d4
Andre Santiago <the.asantiagojr@gmail.com>
2018-06-01 11:53:07 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -7,6 +7,8 @@ def display_board
   puts "   " + "|" + "   " + "|" + "   "
   puts "-----------"
   puts "   " + "|" + "   " + "|" + "   "
+  puts "-----------"
+  puts "   " + "|" + "   " + "|" + "   "
 end
 
 display_board
