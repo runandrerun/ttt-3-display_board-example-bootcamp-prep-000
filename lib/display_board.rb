1f4b0254eaf70179fa126267fca78aab47e1f717
Andre Santiago <the.asantiagojr@gmail.com>
2018-06-01 12:13:43 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,10 +2,10 @@
 
   
 def display_board  
-  print "   " + "|" + "   " + "|" + "   "
-  print "\n-----------\n"
-  print "   " + "|" + "   " + "|" + "   "
-  print "\n-----------\n"
-  print "   " + "|" + "   " + "|" + "   "
+  print "   |   |   
+-----------
+   |   |   
+-----------
+   |   |   
   puts "A Tic Tac Toe Board\n"
 end
